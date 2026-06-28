a90f2917f0c3d0cbf10c80ad9413560a4b057372
LordBhaal <cruz.cristian90@gmail.com>
2026-06-28 02:55:18 -0500

    initial commit

---
+++ ElvUI_MicrobarEnhancement/Locales/ruRU.lua
@@ -0,0 +1,20 @@
+﻿--Файл локализации для ruRU
+local L = ElvUI[1].Libs.ACL:NewLocale("ElvUI", "ruRU")
+if not L then return end
+
+-- Config
+L["Replace icons with letters"] = "Заменить иконки буквами"
+
+-- Symbols
+L["ACHIEVEMENT_SYMBOL"] = "A"
+L["CHARACTER_SYMBOL"] = "C"
+L["GUILD_SYMBOL"] = "G"
+L["HELP_SYMBOL"] = "?"
+L["JOURNAL_SYMBOL"] = "E"
+L["LFD_SYMBOL"] = "D"
+L["LFR_SYMBOL"] = "R"
+L["MENU_SYMBOL"] = "M"
+L["PVP_SYMBOL"] = "P"
+L["QUEST_SYMBOL"] = "Q"
+L["TALENTS_SYMBOL"] = "T"
+L["SPELLBOOK_SYMBOL"] = "S"
---
+++ ElvUI_MicrobarEnhancement/Locales/zhTW.lua
@@ -0,0 +1,20 @@
+-- Taiwanese localization file for zhTW.
+local L = ElvUI[1].Libs.ACL:NewLocale("ElvUI", "zhTW")
+if not L then return end
+
+-- Config
+L["Replace icons with letters"] = true
+
+-- Symbols
+L["ACHIEVEMENT_SYMBOL"] = "A"
+L["CHARACTER_SYMBOL"] = "C"
+L["GUILD_SYMBOL"] = "G"
+L["HELP_SYMBOL"] = "?"
+L["JOURNAL_SYMBOL"] = "E"
+L["LFD_SYMBOL"] = "D"
+L["LFR_SYMBOL"] = "R"
+L["MENU_SYMBOL"] = "M"
+L["PVP_SYMBOL"] = "P"
+L["QUEST_SYMBOL"] = "Q"
+L["TALENTS_SYMBOL"] = "T"
+L["SPELLBOOK_SYMBOL"] = "S"
